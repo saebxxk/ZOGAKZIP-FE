d41687ab3854f116ea157d24b6d99508dae4528b
saebxxk <seoha99@icloud.com>
2024-09-04 12:36:54 +0900

    API fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState }from 'react';
 import AppRouter from './routes/AppRouter';
 import './App.css';
 
 
 
 function App() {
-  return(
+
+
+
+
+  const [data, setData] = useState(null);
+
+
+    useEffect(() => {
+      fetch('https://zogakzip-bmoe.onrender.com', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        setData(data); // 받은 데이터를 상태로 설정
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+    }, []); // 빈 배열로 두면 컴포넌트 마운트 시 한 번만 실행됨
   
+  return(
+
+    
     <div className="App">
       
       <main>
